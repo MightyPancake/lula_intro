9ff63a0dfe6070c814c3993ec80c15a58f59c306
MightyPancake <amightypancake@gmail.com>
2025-01-13 00:31:31 +0100

    Started learning love

--- main.lua
+++ main.lua
@@ -1,9 +1,23 @@
-time = 0
-
 function love.draw()
-    love.graphics.print(time)
+    tg = love.timer.getTime()
+    t = love.timer.getTime()
+    for i=1, 10 do
+        drawCircle(t+i*0.1, tg)
+    end
+end
+
+function drawCircle(t, tg)
+    speed = 5
+    range = 200
+    r_speed = 10
+    r = 15*((math.sin(t*r_speed+tg*5)+1)/2) + 5
+    w = love.graphics.getWidth()
+    h = love.graphics.getHeight()
+    h = h + math.sin(t*speed)*range
+    w = w + math.cos(t*speed)*range
+    love.graphics.circle("fill", w/2, h/2, r)
 end
 
 function love.update(dt)
-    time = time + dt
+    
 end
